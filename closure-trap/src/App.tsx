c5193f6f5aafddd20b982b8d22cf1d1a97c5bfef
iJay <hblx2017@163.com>
2025-04-30 17:09:03 +0800

    closure-trap

---
+++ closure-trap/src/App.tsx
@@ -0,0 +1,95 @@
+import { useEffect, useReducer, useRef, useState } from 'react'
+import './App.css'
+
+interface Data {
+  result: number
+}
+
+interface Action {
+  type: 'add' | 'minus'
+  payload: number
+}
+
+function reducer(state: Data, action: Action) {
+  switch (action.type) {
+    case 'add':
+      return {
+        result: state.result + action.payload
+      }
+    case 'minus':
+      return {
+        result: state.result - action.payload
+      }
+    default:
+      return state
+  }
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const [count1, setCount1] = useState(0)
+
+  const [num, dispatch] = useReducer(reducer, { result: 0})
+
+  // const [num1, setNum1] = useState(0)
+
+  // const ref = useRef()
+
+  useEffect(() => {
+    const timer = setInterval(() => {}, 1000)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
+
+  useEffect(() => {
+    setInterval(() => {
+      // console.log('setInterval', count)
+      setCount((count) => {
+        // console.log('setCount', count)
+        return count + 1
+      })
+    }, 1000)
+  }, [])
+
+  // 每次count1变化都会清除旧的定时器，然后启动新的定时器
+  // 这就导致了定时器不准确
+  useEffect(() => {
+    const timer = setInterval(() => {
+      // console.log('setInterval', count1)
+      setCount1(count1 + 1)
+    }, 1000)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [count1])
+
+  useEffect(() => {
+    setInterval(() => {
+      // console.log('setInterval', num)
+      dispatch(
+        {
+          type: 'add',
+          payload: 1
+        }
+      )
+    }, 1000)
+  }, [])
+
+  return (
+    <>
+      <div className="card">
+        count: {count}
+      </div>
+      <div className="card">
+        count1: {count1}
+      </div>
+      <div className="card">
+        num: {num.result}
+      </div>
+    </>
+  )
+}
+
+export default App
