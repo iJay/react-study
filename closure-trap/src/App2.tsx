a7e92f47f91c9d6ed203b2ea49a7ac9a0ada1dea
iJay <hblx2017@163.com>
2025-05-06 09:59:31 +0800

    closure-trap

--- closure-trap/src/App2.tsx
+++ closure-trap/src/App2.tsx
@@ -1,5 +1,19 @@
-import { useState } from "react"
+import { memo, useState } from "react"
 import useInterval from "./useInterval"
+
+interface ChildProps {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  clean: Function
+}
+
+function Child (props: ChildProps) {
+  const { clean } = props
+  return (
+    <button onClick={() => { clean()}}>stop timer</button>
+  )
+}
+
+const ChildMemo = memo(Child)
 
 function App () {
   const [count, setCount] = useState(0)
@@ -8,11 +22,14 @@ function App () {
     setCount(count + 1)
   }
 
-  useInterval(updateCount, 1000)
+  const cleanUp = useInterval(updateCount, 1000)
 
   return (
     <div>
       count: { count }
+      <div>
+        <ChildMemo clean={cleanUp}/>
+      </div>
     </div>
   )
 }
